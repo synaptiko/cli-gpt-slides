3bdecdc3c5a694b282cfe930a801bc81d02c8db4
Jiří Prokop <jiri@bayzat.com>
2023-04-19 16:17:18 +0200

    Initial version

---
+++ theme/layouts/intro.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { handleBackground } from '../layoutHelper'
+const props = defineProps({
+  image: {
+    type: String,
+  },
+})
+const style = computed(() => handleBackground(props.image))
+</script>
+
+<template>
+  <div class="slidev-layout intro" :style="style">
+    <div class="my-auto">
+      <slot />
+    </div>
+  </div>
+</template>
